41cfa12190a69b874803e4808ba48149ca860004
张云熙 <135599587+01lv02@users.noreply.github.com>
2023-06-14 19:54:23 +0800

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -8,6 +8,7 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.core import Flatten
 from keras.optimizers import SGD
 from keras.datasets import mnist
+from keras import backend as K
 import numpy as np
 from PIL import Image
 import argparse
